88514126b742b727a9f13d035dd6ecb249488f94
okyshkevych <okyshkevych@scalhive.com>
2021-10-09 15:55:19 +0300

    Iteration 1: added communication prototype for Master

--- src/main/scala/InMemoryStorage.scala
+++ src/main/scala/InMemoryStorage.scala
@@ -1,13 +1,36 @@
+import akka.actor.typed.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpEntity, HttpMethods, HttpRequest, Uri}
+
 import scala.collection.mutable
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
-class InMemoryStorage(implicit executionContext: ExecutionContextExecutor) {
+class InMemoryStorage(implicit actorSystem: ActorSystem[_], executionContext: ExecutionContextExecutor) {
   val data: mutable.SortedMap[Int, String] = mutable.SortedMap.empty
   var counter: Int                         = 0
 
-  def store(newData: String): Future[mutable.SortedMap[Int, String]] = Future {
-    data += counter -> newData
-    counter += 1
-    data
-  }
+  //Uri for secondaries (name explanation https://www.youtube.com/watch?v=dQw4w9WgXcQ)
+  val RickUri: Uri  = Uri("localhost/data:1861")
+  val AshlyUri: Uri = Uri("localhost/data:1862")
+
+  def store(newData: String): Future[mutable.SortedMap[Int, String]] =
+    for {
+      data <- Future {
+               data += counter -> newData
+               counter += 1
+               data
+             }
+      _ <- Http().singleRequest(
+            HttpRequest(
+              method = HttpMethods.POST,
+              uri = RickUri,
+              entity = HttpEntity(newData)
+            ))
+      _ <- Http().singleRequest(
+            HttpRequest(
+              method = HttpMethods.POST,
+              uri = AshlyUri,
+              entity = HttpEntity(newData)
+            ))
+    } yield data
 }
