7554c4046bc0e7a72b4bad4caa54d0a02c1a4d6a
DmytroIlchuk <dmytro.ilchuk@ring.com>
2021-10-11 01:00:18 +0300

    Separating Private and Public routs

--- src/main/scala/Secondary.scala
+++ src/main/scala/Secondary.scala
@@ -4,15 +4,17 @@ import com.typesafe.scalalogging.LazyLogging
 
 import scala.concurrent.ExecutionContextExecutor
 
-object Master extends App with LazyLogging {
+object Secondary extends App with LazyLogging {
 
   val rootBehavior = Behaviors.setup[Nothing] { context =>
 
     implicit val actorSystem: ActorSystem[Nothing] = context.system
     implicit val executionContext: ExecutionContextExecutor = context.system.executionContext
 
-    val inMemoryStorage = new InMemoryStorage
-    val applicationRoutes = new ApplicationRoutes(inMemoryStorage)
+    val inMemoryStorage   = new InMemoryStorage
+    val privateRoutes     = new PrivateRoutes(inMemoryStorage)
+    val publicRoutes      = new PublicRoutes(inMemoryStorage)
+    val applicationRoutes = new ApplicationRoutes(privateRoutes, publicRoutes)
 
     new Server(applicationRoutes).start(1337)
     Behaviors.empty
