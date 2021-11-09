c2f06f623ef3d0f7b0587f4f10b405375994ddd8
okyshkevych <okyshkevych@scalhive.com>
2021-11-09 12:34:49 +0200

    Iteration 2: add artificial delay

--- src/main/scala/InMemoryStorage.scala
+++ src/main/scala/InMemoryStorage.scala
@@ -7,9 +7,11 @@ import scala.concurrent.ExecutionContextExecutor
 
 class InMemoryStorage(implicit actorSystem: ActorSystem[_], executionContext: ExecutionContextExecutor) extends LazyLogging {
   val data: mutable.SortedMap[Int, String] = mutable.SortedMap.empty
-  var counter: Int                         = 0
+  var delay: Int                           = 0
 
   def store(message: String): Unit = {
+    Thread.sleep(delay)
+
     val jsonData = Json.parse(message)
     val id       = (jsonData \ "id").as[Int]
     val newData  = (jsonData \ "data").as[String]
@@ -18,5 +20,8 @@ class InMemoryStorage(implicit actorSystem: ActorSystem[_], executionContext: Ex
     logger.info("json is {} id is {} and data is {}", message, id, newData)
   }
 
+  //TODO: better to change storing new messages in temporary buffer like in FIFO Replication than this O(4n) crap
   def showData: String = data.zipWithIndex.takeWhile(d => d._1._1 == d._2).map(_._1._2).mkString("\n")
+
+  def setDelay(d: Int): Unit = delay = d
 }
--- src/main/scala/PrivateRoutes.scala
+++ src/main/scala/PrivateRoutes.scala
@@ -1,16 +1,25 @@
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import play.api.libs.json.JsValue
 
 class PrivateRoutes(inMemoryStorage: InMemoryStorage) {
   val route: Route =
-    path("store") {
-      post {
-        entity(as[String]) { data =>
-          inMemoryStorage.store(data)
-          complete(StatusCodes.OK)
+    concat (
+      path("store") {
+        post {
+          entity(as[String]) { data =>
+            inMemoryStorage.store(data)
+            complete(StatusCodes.OK)
+          }
+        }
+      },
+      path("delay"){
+        post{
+          entity(as[String]){delay =>
+            inMemoryStorage.setDelay(delay.toIntOption.getOrElse(0))
+            complete(StatusCodes.OK)
+          }
         }
       }
-    }
+    )
 }
